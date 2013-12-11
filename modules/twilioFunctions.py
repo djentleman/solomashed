2edc61e25a5149ef211f52e55d82dec7f0d6ab4b
benjanyan <ben@bmcq.co.uk>
2013-12-11 12:54:04 +0000

    Twilio phone number test

--- modules/twilioFunctions.py
+++ modules/twilioFunctions.py
@@ -2,22 +2,14 @@ from flask import Flask, request, redirect
 import twilio.twiml
  
 app = Flask(__name__)
-
+ 
 @app.route("/", methods=['GET', 'POST'])
 def processRequest():
-    """Respond to incoming texts"""
+    """Respond to incoming calls with a simple text message."""
+ 
     resp = twilio.twiml.Response()
-    resp.message = (str(request))
-    mesg = twilio.twiml.Message()
+    resp.message(str(request.form['from']))
     return str(resp)
-
-@app.route("/hi", methods=['GET', 'POST'])
-def shit():
-	resp = twilio.twiml.Response()
-	mesg = twilio.twiml.Message()
-	resp.message("Foloffle")	
-	return str(resp)
-
-
+ 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(debug=True)
