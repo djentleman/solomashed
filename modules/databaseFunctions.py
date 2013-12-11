053f1a7210cc0d64368f68d1d6724f5252852750
rasengangsta <up613235@myport.ac.uk>
2013-12-11 11:59:38 +0000

    Update databaseFunctions.py

--- modules/databaseFunctions.py
+++ modules/databaseFunctions.py
@@ -17,11 +17,44 @@ def createTables(con):
     c.execute("DROP TABLE IF EXISTS Drinks")
     c.execute("DROP TABLE IF EXISTS Measures")
     c.execute("CREATE TABLE Users(pNumber TEXT, Name TEXT)")
-    c.execute("CREATE TABLE Drinks(DrinkID INT, Name TEXT, Percentage INT)")
-    c.execute("CREATE TABLE Measures(measureID INT, Name TEXT, MeasureMl INT)")
+    c.execute("CREATE TABLE Drinks(Name TEXT, Percentage INT)")
+    c.execute("CREATE TABLE Measures(Name TEXT, MeasureMl INT)")
     con.commit()
+
+def populateDrinks(con):
+    c = con.cursor()
+    c.executescript("""
+        INSERT INTO Drinks VALUES('beer', 4);
+        INSERT INTO Drinks VALUES('wine', 13);
+        INSERT INTO Drinks VALUES('vodka', 40);
+        """)
+    con.commit()
+
+def populateMeasures(con):
+    c = con.cursor()
+    c.executescript("""
+        INSERT INTO Measures VALUES('pint', 568);
+        INSERT INTO Measures VALUES('shot', 44);
+        INSERT INTO Measures VALUES('litre', 1000);
+        """)
+    con.commit()
+
+
+
+def getAllDrinks(con):
+    c = con.cursor()
+    c.execute("SELECT d.rowid, d.* FROM Drinks d")
+    rows = c.fetchall()
+    return rows
+    
+
 
 con = generateConnection()
 createTables(con)
+populateDrinks(con)
+
+
+
+
 
 
