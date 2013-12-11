d49aed2f75237f7f5a123c499727a179a1e77537
benjanyan <ben@bmcq.co.uk>
2013-12-11 12:40:21 +0000

    Twilio update

--- modules/twilioFunctions.py
+++ modules/twilioFunctions.py
@@ -2,14 +2,22 @@ from flask import Flask, request, redirect
 import twilio.twiml
  
 app = Flask(__name__)
- 
+
 @app.route("/", methods=['GET', 'POST'])
 def processRequest():
     """Respond to incoming texts"""
- 
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message = (str(request))
+    mesg = twilio.twiml.Message()
     return str(resp)
- 
+
+@app.route("/hi", methods=['GET', 'POST'])
+def shit():
+	resp = twilio.twiml.Response()
+	mesg = twilio.twiml.Message()
+	resp.message("Foloffle")	
+	return str(resp)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
